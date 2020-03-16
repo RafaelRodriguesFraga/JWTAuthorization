0f84b12e0550cbf8050edc2a03b2d0081e70c70d
Rafael Rodrigues Fraga <rafaelfraga2591@hotmail.com>
2020-03-16 17:25:35 -0300

    Created UserRepositpry and AuthController

---
+++ JWTAuthorization/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using JWTAuthorization.Models;
+using JWTAuthorization.Repositories;
+using JWTAuthorization.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthorization.Controllers
+{
+    [Route("v1/api/account")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public ActionResult<dynamic> Authenticate([FromBody] User user)
+        {
+            User userData = UserRepository.GetUser(user.Username, user.Password);
+
+            if (userData == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            var token = TokenService.GenerateToken(userData);
+            userData.Password = "";
+
+            return new
+            {
+                userData,
+                token
+
+            };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous()
+        {
+            return "Anonymous";
+        }
+
+        [HttpGet]
+        [Route("authenticated")]    
+        [Authorize]
+        public string Authenticated()
+        {
+            return string.Format("Authenticated - {0}", User.Identity.Name);
+        }
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee, manager")]
+        public string Employee()
+        {
+            return "Employees";
+        }
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager()
+        {
+            return "Managers";
+        }
+
+
+    }
+}
---
+++ JWTAuthorization/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using JWTAuthorization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JWTAuthorization.Repositories
+{
+    public static class UserRepository
+    {
+        public static User GetUser(string username, string paswword)
+        {
+            List<User> users = new List<User>();        
+            users.Add(new User { Id = 1, Username = "Dean Winchester", Password = "Supernatural", Role = "manager"});
+            users.Add(new User { Id = 2, Username = "Sam Winchester", Password = "Supernatural", Role = "employee" });
+
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password.ToLower() == paswword.ToLower()).FirstOrDefault();
+
+        }
+    }
+}
